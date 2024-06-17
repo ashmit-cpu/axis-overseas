4ab1922fc637ca654e11b81a3a70913b05415ff3
Ashmit k <ashmithkacheri138@gmail.com>
2024-06-17 15:34:49 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { MenuProvider } from './Context/menuProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MenuProvider>
       <App />
-    </MenuProvider>
   </React.StrictMode>
 );
 
